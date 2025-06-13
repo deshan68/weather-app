0ab024a045585d91887ad95e90f014c3e6bf13a0
arundeshan@gmail.com <arundeshan@gmail.com>
2025-06-14 00:05:25 +0530

    feat: integrate redux-persist for state persistence and enhance store configuration

--- src/store/store.ts
+++ src/store/store.ts
@@ -1,11 +1,28 @@
-import { configureStore } from "@reduxjs/toolkit";
+import { configureStore, combineReducers } from "@reduxjs/toolkit";
 import weatherReducer from "./slices/weatherSlice";
+import cityPreferencesReducer from "./slices/cityPreferencesSlice";
+import storage from "redux-persist/lib/storage";
+import { persistReducer, persistStore } from "redux-persist";
+
+const rootReducer = combineReducers({
+  weather: weatherReducer,
+  cityPreferences: cityPreferencesReducer,
+});
+
+const persistedReducer = persistReducer(
+  { key: "root", storage, whitelist: ["cityPreferences"] },
+  rootReducer
+);
 
 export const store = configureStore({
-  reducer: {
-    weather: weatherReducer,
-  },
+  reducer: persistedReducer,
+  middleware: (getDefaultMiddleware) =>
+    getDefaultMiddleware({
+      serializableCheck: false,
+    }),
 });
+
+export const persistor = persistStore(store);
 
 export type RootState = ReturnType<typeof store.getState>;
 export type AppDispatch = typeof store.dispatch;
