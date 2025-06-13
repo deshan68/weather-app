d83559e1b6d1ab957577fd286209c260ef99915f
arundeshan@gmail.com <arundeshan@gmail.com>
2025-06-13 22:14:56 +0530

    fix: update MarkerDropDown to include current weather location and improve styling

--- src/components/navbar/MarkerDropDown.tsx
+++ src/components/navbar/MarkerDropDown.tsx
@@ -4,10 +4,13 @@ import {
   DropdownMenuContent,
   DropdownMenuTrigger,
 } from "@/components/ui/dropdown-menu";
-import { MapPinned, Trash } from "lucide-react";
+import { MapPinned, Pin, Trash } from "lucide-react";
 import { Text } from "../ui/text";
+import { useAppSelector } from "@/hooks/useRedux";
 
 export function MarkerDropDown() {
+  const { currentWeather } = useAppSelector((state) => state.weather);
+
   return (
     <DropdownMenu>
       <DropdownMenuTrigger asChild>
@@ -17,18 +20,23 @@ export function MarkerDropDown() {
       </DropdownMenuTrigger>
       <DropdownMenuContent
         align="end"
-        className="w-[200px] gap-2 py-2 flex flex-col "
+        className="w-[200px] gap-2 py-2 flex flex-col"
       >
         {["Colombo", "Jaffna", "Matara", "Galle"].map((c, index) => (
           <div key={index} className="flex items-center justify-between px-3">
-            <Text size={"sm"} weight={"light"}>
+            <Text size={"sm"} weight={"light"} color={"primary"}>
               {c}
             </Text>
-            <Button variant="ghost" className="text-destructive" size="icon">
+            <Button variant="ghost" size="icon">
               <Trash />
             </Button>
           </div>
         ))}
+
+        <Button variant="default" className="mt-2 mx-1" size="sm">
+          <Pin className="size-3.5" />
+          {currentWeather?.location.name}
+        </Button>
       </DropdownMenuContent>
     </DropdownMenu>
   );
