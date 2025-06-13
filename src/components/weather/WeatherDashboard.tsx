4b6febb8db4f7803de69cd0bb38d806b3f7682ee
arundeshan@gmail.com <arundeshan@gmail.com>
2025-06-13 15:00:02 +0530

    refactor: reorganize layout structure in WeatherDashboard component

--- src/components/weather/WeatherDashboard.tsx
+++ src/components/weather/WeatherDashboard.tsx
@@ -2,6 +2,7 @@ import { useEffect } from "react";
 import { MainWeatherDetailsArea } from "./MainWeatherDetailsArea";
 import { useWeatherData } from "@/hooks/useWeatherData";
 import ChanceOfRainArea from "./ChanceOfRainArea";
+import MapArea from "../map/MapArea";
 
 function WeatherDashboard() {
   const { fetchWeatherByLocation } = useWeatherData();
@@ -11,13 +12,22 @@ function WeatherDashboard() {
   }, []);
 
   return (
-    <div className="flex w-full flex-col gap-2 md:flex-row">
-      <div className="w-full md:w-2/3">
-        <MainWeatherDetailsArea />
+    <div className="flex w-full gap-2 flex-col">
+      <div className="flex w-full flex-col gap-2 md:flex-row">
+        <div className="w-full md:w-2/3">
+          <MainWeatherDetailsArea />
+        </div>
+
+        <div className="flex w-full md:w-1/3">
+          <ChanceOfRainArea />
+        </div>
       </div>
 
-      <div className="flex w-full md:w-1/3">
-        <ChanceOfRainArea />
+      <div className="flex w-full flex-col gap-2 md:flex-row">
+        <div className="w-full md:w-2/3">
+          <MapArea />
+        </div>
+        <div className="flex w-full md:w-1/3">Not implemented yet</div>
       </div>
     </div>
   );
