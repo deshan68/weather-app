1e2f102a54247c7133773c761f182835a361cd96
arundeshan@gmail.com <arundeshan@gmail.com>
2025-06-13 09:10:13 +0530

    feat: implement WeatherDashboard component to fetch and display weather data

--- src/components/weather/WeatherDashboard.tsx
+++ src/components/weather/WeatherDashboard.tsx
@@ -1,5 +1,26 @@
+import { useEffect } from "react";
+import { Text } from "../ui/text";
+import { MainWeatherDetailsArea } from "./MainWeatherDetailsArea";
+import { useWeatherData } from "@/hooks/useWeatherData";
+
 function WeatherDashboard() {
-  return <div>WeatherDashboard</div>;
+  const { fetchWeatherByLocation } = useWeatherData();
+  useEffect(() => {
+    fetchWeatherByLocation("colombo");
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, []);
+
+  return (
+    <div className="flex w-full flex-col gap-2 md:flex-row">
+      <div className="w-full md:w-2/3">
+        <MainWeatherDetailsArea />
+      </div>
+
+      <div className="flex w-full md:w-1/3 bg-accent">
+        <Text size={"sm"}>Chance of rain</Text>
+      </div>
+    </div>
+  );
 }
 
 export default WeatherDashboard;
