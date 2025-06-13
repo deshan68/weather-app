77009106b94da8d012cd85516c0924fa46d0db08
arundeshan@gmail.com <arundeshan@gmail.com>
2025-06-14 00:05:45 +0530

    feat: add city preferences slice and types for managing favorite and pinned cities

---
+++ src/store/slices/cityPreferencesSlice.ts
@@ -0,0 +1,63 @@
+import type { FavoriteCity, PinnedCity } from "@/types/cityPreference";
+import { createSlice, type PayloadAction } from "@reduxjs/toolkit";
+
+interface CityPreferencesState {
+  favoriteCities: FavoriteCity[];
+  pinnedCities: PinnedCity[];
+}
+
+const initialState: CityPreferencesState = {
+  favoriteCities: [],
+  pinnedCities: [],
+};
+
+const cityPreferencesSlice = createSlice({
+  name: "cityPreferences",
+  initialState,
+  reducers: {
+    addFavoriteCity(state, action: PayloadAction<FavoriteCity>) {
+      if (
+        !state.favoriteCities.find((city) => city.name === action.payload.name)
+      ) {
+        const newCity: FavoriteCity = {
+          name: action.payload.name,
+        };
+        const updatedList = [...state.favoriteCities, newCity];
+        state.favoriteCities = updatedList;
+      }
+    },
+    removeFavoriteCity(state, action: PayloadAction<string>) {
+      state.favoriteCities = state.favoriteCities.filter(
+        (city) => city.name !== action.payload
+      );
+    },
+    addPinnedCity(state, action: PayloadAction<PinnedCity>) {
+      if (
+        !state.pinnedCities.find((city) => city.name === action.payload.name)
+      ) {
+        const newCity: PinnedCity = {
+          name: action.payload.name,
+          icon: action.payload.icon,
+          lat: action.payload.lat,
+          lon: action.payload.lon,
+        };
+        const updatedList = [...state.pinnedCities, newCity];
+        state.pinnedCities = updatedList;
+      }
+    },
+    removePinnedCity(state, action: PayloadAction<string>) {
+      state.pinnedCities = state.pinnedCities.filter(
+        (city) => city.name !== action.payload
+      );
+    },
+  },
+});
+
+export const {
+  addFavoriteCity,
+  removeFavoriteCity,
+  addPinnedCity,
+  removePinnedCity,
+} = cityPreferencesSlice.actions;
+
+export default cityPreferencesSlice.reducer;
---
+++ src/types/cityPreference.ts
@@ -0,0 +1,9 @@
+export interface FavoriteCity {
+  name: string;
+}
+export interface PinnedCity {
+  name: string;
+  icon: string;
+  lat: number;
+  lon: number;
+}
