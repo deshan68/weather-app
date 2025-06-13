232006c00c43ecc5b40055eb50458ca741f61321
arundeshan@gmail.com <arundeshan@gmail.com>
2025-06-14 00:05:59 +0530

    feat: update MapArea and MarkerDropDown components

--- src/components/map/MapArea.tsx
+++ src/components/map/MapArea.tsx
@@ -7,6 +7,7 @@ import { Text } from "../ui/text";
 
 function MapArea() {
   const { theme } = useTheme();
+  const { pinnedCities } = useAppSelector((state) => state.cityPreferences);
   const weatherDataList = useAppSelector(
     (state) => state.weather.currentWeather?.forecast?.forecastday
   );
@@ -30,15 +31,22 @@ function MapArea() {
         mapStyle={mapStyle}
         style={{ width: "100%", height: "50vh", borderRadius: "1.5rem" }}
       >
-        <Marker longitude={79.8612} latitude={6.9271} anchor="top">
-          <div className="flex p-1 items-center justify-center bg-background rounded-full border border-sidebar-ring">
-            <img
-              src={`https:${weatherDataList[0].day.condition.icon}`}
-              alt={weatherDataList[0].day.condition.text}
-              className="w-6 h-6"
-            />
-          </div>
-        </Marker>
+        {pinnedCities.map((city, index) => (
+          <Marker
+            key={index}
+            longitude={city.lon}
+            latitude={city.lat}
+            anchor="top"
+          >
+            <div className="flex p-1 items-center justify-center bg-background rounded-full border border-sidebar-ring">
+              <img
+                src={`https:${city.icon}`}
+                alt={city.name}
+                className="w-5 h-5"
+              />
+            </div>
+          </Marker>
+        ))}
       </Map>
     </div>
   );
--- src/components/navbar/MarkerDropDown.tsx
+++ src/components/navbar/MarkerDropDown.tsx
@@ -6,10 +6,32 @@ import {
 } from "@/components/ui/dropdown-menu";
 import { MapPinned, Pin, Trash } from "lucide-react";
 import { Text } from "../ui/text";
-import { useAppSelector } from "@/hooks/useRedux";
+import { useAppDispatch, useAppSelector } from "@/hooks/useRedux";
+import {
+  addPinnedCity,
+  removePinnedCity,
+} from "@/store/slices/cityPreferencesSlice";
 
 export function MarkerDropDown() {
+  const dispatch = useAppDispatch();
   const { currentWeather } = useAppSelector((state) => state.weather);
+  const { pinnedCities } = useAppSelector((state) => state.cityPreferences);
+
+  if (!currentWeather) return null;
+
+  const handlePinCity = () => {
+    dispatch(
+      addPinnedCity({
+        icon: currentWeather.current.condition.icon,
+        name: currentWeather.location.name,
+        lat: currentWeather.location.lat,
+        lon: currentWeather.location.lon,
+      })
+    );
+  };
+  const handleUnpinCity = () => {
+    dispatch(removePinnedCity(currentWeather.location.name));
+  };
 
   return (
     <DropdownMenu>
@@ -20,20 +42,36 @@ export function MarkerDropDown() {
       </DropdownMenuTrigger>
       <DropdownMenuContent
         align="end"
-        className="w-[200px] gap-2 py-2 flex flex-col"
+        className="w-[200px] min-h-24 justify-between gap-1 py-2 flex flex-col"
       >
-        {["Colombo", "Jaffna", "Matara", "Galle"].map((c, index) => (
-          <div key={index} className="flex items-center justify-between px-3">
-            <Text size={"sm"} weight={"light"} color={"primary"}>
-              {c}
-            </Text>
-            <Button variant="ghost" size="icon">
-              <Trash />
-            </Button>
-          </div>
-        ))}
+        {pinnedCities.length === 0 ? (
+          <Text size={"xs"} color={"muted"} className="text-center">
+            No pinned cities.
+          </Text>
+        ) : (
+          <>
+            {pinnedCities.map((c, index) => (
+              <div
+                key={index}
+                className="flex items-center justify-between px-2"
+              >
+                <Text size={"sm"} weight={"light"} color={"primary"}>
+                  {c.name}
+                </Text>
+                <Button variant="ghost" size="icon" onClick={handleUnpinCity}>
+                  <Trash className="size-3.5" />
+                </Button>
+              </div>
+            ))}
+          </>
+        )}
 
-        <Button variant="default" className="mt-2 mx-1" size="sm">
+        <Button
+          variant="default"
+          className="mt-2 mx-1"
+          size="sm"
+          onClick={handlePinCity}
+        >
           <Pin className="size-3.5" />
           {currentWeather?.location.name}
         </Button>
