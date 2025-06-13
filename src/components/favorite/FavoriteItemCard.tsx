47c7b79ce515e7e9abfc8d8e5fdc29a4534c54ee
arundeshan@gmail.com <arundeshan@gmail.com>
2025-06-13 16:28:45 +0530

    feat: add FavoriteItemCard and FavoriteItemsArea components

---
+++ src/components/favorite/FavoriteItemCard.tsx
@@ -0,0 +1,33 @@
+import { Text } from "../ui/text";
+
+function FavoriteItemCard() {
+  return (
+    <div className="flex w-full bg-accent rounded-3xl p-4">
+      <div className="flex flex-col">
+        <Text weight={"light"} size={"xs"} color={"muted"}>
+          Sri Lanka
+        </Text>
+        <Text size={"2xl"} weight={"medium"}>
+          Colombo
+        </Text>
+        <Text size={"xs"} weight={"light"} className="mt-auto">
+          Mostly Sunny
+        </Text>
+      </div>
+
+      {/* image and temp */}
+      <div className="flex items-center justify-center flex-col ml-auto">
+        <img
+          src="https://cdn.weatherapi.com/weather/64x64/day/113.png"
+          alt="Weather Icon"
+          className="w-14 h-14"
+        />
+        <Text size={"lg"} weight={"medium"} className="ml-2">
+          30°
+        </Text>
+      </div>
+    </div>
+  );
+}
+
+export default FavoriteItemCard;
