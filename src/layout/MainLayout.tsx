d12c067f0ae66f7b75633527bc3dad95bbee9a77
arundeshan@gmail.com <arundeshan@gmail.com>
2025-06-14 12:53:48 +0530

    fix: remove redundant padding for improved layout in MainLayout component

--- src/layout/MainLayout.tsx
+++ src/layout/MainLayout.tsx
@@ -3,7 +3,7 @@ import React from "react";
 
 function MainLayout({ children }: { children: React.ReactNode }) {
   return (
-    <div className="min-h-screen mx-auto max-w-5xl bg-background px-4 md:px-4">
+    <div className="min-h-screen mx-auto max-w-5xl bg-background px-4">
       <Navbar />
       <main>{children}</main>
     </div>
