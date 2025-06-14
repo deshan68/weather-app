59c356107bd210915cf1878289174d4c52e55112
arundeshan@gmail.com <arundeshan@gmail.com>
2025-06-14 23:48:50 +0530

    fix: restructure layout of MainWeatherDetailsArea for improved tab display

--- src/components/weather/MainWeatherDetailsArea.tsx
+++ src/components/weather/MainWeatherDetailsArea.tsx
@@ -1,14 +1,19 @@
 import { Tabs, TabsContent, TabsList, TabsTrigger } from "@/components/ui/tabs";
 import TabContent from "./TabContent";
+import CalendarResultDrawer from "./CalendarResultDrawer";
 
 export function MainWeatherDetailsArea() {
   return (
     <Tabs defaultValue="today" className="flex max-w-sm flex-col min-w-full">
-      <TabsList>
-        <TabsTrigger value="today">Today</TabsTrigger>
-        <TabsTrigger value="tomorrow">Tomorrow</TabsTrigger>
-        <TabsTrigger value="sevenDays">Next 7 days</TabsTrigger>
-      </TabsList>
+      <div className="flex items-center gap-x-2">
+        <TabsList className="flex">
+          <TabsTrigger value="today">Today</TabsTrigger>
+          <TabsTrigger value="tomorrow">Tomorrow</TabsTrigger>
+          <TabsTrigger value="sevenDays">Next 7 days</TabsTrigger>
+        </TabsList>
+        <CalendarResultDrawer />
+      </div>
+
       <TabsContent value="today">
         <TabContent subCardListType="today" />
       </TabsContent>
