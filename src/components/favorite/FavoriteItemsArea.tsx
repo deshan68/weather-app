b61075e5004197e24626330e0cf86fb18c168273
arundeshan@gmail.com <arundeshan@gmail.com>
2025-06-15 10:01:52 +0530

    refactor: remove unused error handling for currentWeather in FavoriteItemsArea and ChanceOfRainArea components

--- src/components/favorite/FavoriteItemsArea.tsx
+++ src/components/favorite/FavoriteItemsArea.tsx
@@ -2,21 +2,12 @@ import { getUpcomingHourlyPredictions } from "@/utils/weatherHelpers";
 import AppBarChart from "../charts/AppBarChart";
 import { useAppSelector } from "@/hooks/useRedux";
 import AppAreaChart from "../charts/AppAreaChart";
-import { Text } from "../ui/text";
 
 function FavoriteItemsArea() {
   const { currentWeather, temperatureUnit } = useAppSelector(
     (state) => state.weather
   );
 
-  if (!currentWeather)
-    return (
-      <div className="flex gap-x-2 w-full justify-center items-center h-[50vh] border border-separate rounded-3xl p-4">
-        <Text size="sm" className="italic" color={"muted"}>
-          No weather data available
-        </Text>
-      </div>
-    );
   return (
     <div className="flex flex-col gap-1 h-full w-full">
       <AppAreaChart
--- src/components/weather/ChanceOfRainArea.tsx
+++ src/components/weather/ChanceOfRainArea.tsx
@@ -1,20 +1,9 @@
 import { useAppSelector } from "@/hooks/useRedux";
 import { getUpcomingHourlyPredictions } from "@/utils/weatherHelpers";
 import AppBarChart from "../charts/AppBarChart";
-import { Text } from "../ui/text";
 
 function ChanceOfRainArea() {
   const { currentWeather } = useAppSelector((state) => state.weather);
-
-  if (!currentWeather || !currentWeather.forecast) {
-    return (
-      <div className="flex items-center justify-center h-full min-h-52 w-full border rounded-3xl">
-        <Text size="sm" className="italic" color={"muted"}>
-          No weather data available
-        </Text>
-      </div>
-    );
-  }
 
   return (
     <div className="h-full w-full">
