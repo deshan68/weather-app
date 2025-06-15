0ca01ec54501156be1e442f8cdc4ce317a4b1c8f
arundeshan@gmail.com <arundeshan@gmail.com>
2025-06-15 11:25:10 +0530

    refactor: enhance DateWiseWeatherDisplay to include selected day weather data and improve data extraction logic

--- src/components/weather/DateWiseWeatherDisplay.tsx
+++ src/components/weather/DateWiseWeatherDisplay.tsx
@@ -1,5 +1,5 @@
 import { useCallback, useEffect, useState } from "react";
-import { format, addDays } from "date-fns";
+import { format, addDays, isToday } from "date-fns";
 import { cn } from "@/lib/utils";
 import { Text } from "../ui/text";
 import { useAppSelector } from "@/hooks/useRedux";
@@ -7,6 +7,7 @@ import AppAreaChart from "../charts/AppAreaChart";
 import AppBarChart from "../charts/AppBarChart";
 import { DAYS_TO_SHOW } from "@/lib/constants";
 import type { HourlyWeatherData } from "@/types";
+import WeatherMainCard, { type WeatherDataProps } from "./WeatherMainCard";
 
 function DateWiseWeatherDisplay() {
   const { currentWeather, temperatureUnit, error } = useAppSelector(
@@ -16,6 +17,8 @@ function DateWiseWeatherDisplay() {
   const today = new Date();
   const [selectedDate, setSelectedDate] = useState<Date>(today);
   const [hourlyData, setHourlyData] = useState<HourlyWeatherData[]>([]);
+  const [selectedDayWeather, setSelectedDayWeather] =
+    useState<WeatherDataProps | null>(null);
 
   const forecastDays = currentWeather?.forecast?.forecastday || [];
 
@@ -23,40 +26,92 @@ function DateWiseWeatherDisplay() {
     addDays(today, i)
   );
 
-  const extractHourlyData = useCallback(
-    (date: Date) => {
-      const dayData = forecastDays.find(
+  const extractHourlyData = useCallback((date: Date) => {
+    const dayData = forecastDays.find(
+      (day) =>
+        format(new Date(day.date), "yyyy-MM-dd") === format(date, "yyyy-MM-dd")
+    );
+    if (!dayData) return [];
+    return dayData.hour.map((hour) => ({
+      time: format(new Date(hour.time), "HH:mm"),
+      temp: temperatureUnit === "celsius" ? hour.temp_c : hour.temp_f,
+      windSpeed: temperatureUnit === "celsius" ? hour.wind_kph : hour.wind_mph,
+      humidity: hour.humidity,
+    }));
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, []);
+
+  const extractDayWeatherData = useCallback(
+    (date: Date): WeatherDataProps | null => {
+      if (!currentWeather) return null;
+
+      if (isToday(date)) {
+        return {
+          ...currentWeather.current,
+          localtime: currentWeather.location.localtime,
+        };
+      }
+
+      const selectedDay = forecastDays.find(
         (day) =>
           format(new Date(day.date), "yyyy-MM-dd") ===
           format(date, "yyyy-MM-dd")
       );
 
-      if (!dayData) return [];
+      if (!selectedDay) return null;
 
-      return dayData.hour.map((hour) => ({
-        time: format(new Date(hour.time), "HH:mm"),
-        temp: temperatureUnit === "celsius" ? hour.temp_c : hour.temp_f,
-        windSpeed:
-          temperatureUnit === "celsius" ? hour.wind_kph : hour.wind_mph,
-        humidity: hour.humidity,
-      }));
+      const {
+        avgtemp_c,
+        avgtemp_f,
+        avghumidity,
+        maxwind_kph,
+        maxwind_mph,
+        avgvis_km,
+        avgvis_miles,
+        condition,
+        uv,
+      } = selectedDay.day;
+
+      return {
+        localtime: `${selectedDay.date} 12:00`, // Midday
+        temp_c: avgtemp_c,
+        temp_f: avgtemp_f,
+        feelslike_c: avgtemp_c,
+        feelslike_f: avgtemp_f,
+        humidity: avghumidity,
+        wind_kph: maxwind_kph,
+        wind_mph: maxwind_mph,
+        uv: uv,
+        cloud: selectedDay.day.daily_chance_of_rain,
+        vis_km: avgvis_km,
+        vis_miles: avgvis_miles,
+        condition: condition,
+      };
     },
     // eslint-disable-next-line react-hooks/exhaustive-deps
-    [temperatureUnit]
+    []
   );
 
   const handleDateClick = (date: Date) => {
     setSelectedDate(date);
     setHourlyData(extractHourlyData(date));
+    setSelectedDayWeather(extractDayWeatherData(date));
   };
 
   useEffect(() => {
     setHourlyData(extractHourlyData(today));
+    setSelectedDayWeather(extractDayWeatherData(today));
     // eslint-disable-next-line react-hooks/exhaustive-deps
-  }, [currentWeather, temperatureUnit]);
+  }, [
+    currentWeather,
+    temperatureUnit,
+    extractHourlyData,
+    extractDayWeatherData,
+  ]);
 
   return (
     <div className="flex flex-col overflow-y-auto p-2">
+      {/* Date Picker */}
       <div className="flex gap-2 pt-4 pb-2 justify-between">
         {dates.map((date) => {
           const isSelected =
@@ -69,7 +124,7 @@ function DateWiseWeatherDisplay() {
               className="flex flex-col items-center justify-center gap-y-1"
             >
               <Text size="xs" color={isSelected ? "primary" : "muted"}>
-                {format(date, "EEEEE")} {/* Day: S M T */}
+                {format(date, "EEEEE")}
               </Text>
               <Text
                 size="xs"
@@ -79,7 +134,7 @@ function DateWiseWeatherDisplay() {
                   isSelected && "bg-primary text-primary-foreground"
                 )}
               >
-                {format(date, "d")} {/* Day number */}
+                {format(date, "d")}
               </Text>
             </button>
           );
@@ -98,8 +153,12 @@ function DateWiseWeatherDisplay() {
         </div>
       )}
 
-      {hourlyData.length > 0 && (
+      {hourlyData.length > 0 && selectedDayWeather && (
         <div className="flex flex-col gap-4 py-4 mt-2">
+          <WeatherMainCard
+            weatherData={selectedDayWeather}
+            temperatureUnit={temperatureUnit}
+          />
           <AppAreaChart
             data={hourlyData}
             keyOfXAxis="time"
