1111f8e7b4694441ae736d90d7dcf8de973345ed
arundeshan@gmail.com <arundeshan@gmail.com>
2025-06-16 16:45:31 +0530

    feat: add UVIndexCard and WindInfoCard components to display weather information

---
+++ src/components/weather/WindInfoCard.tsx
@@ -0,0 +1,118 @@
+import { Text } from "../ui/text";
+import { useAppSelector } from "@/hooks/useRedux";
+
+interface WindInfoCardProps {
+  windMph: number;
+  windKph: number;
+  windDegree: number;
+  gustMph: number;
+  gustKph: number;
+}
+
+export const WindInfoCard = ({
+  windMph,
+  windKph,
+  windDegree,
+  gustMph,
+  gustKph,
+}: WindInfoCardProps) => {
+  const RADIUS = 48;
+  const DOT_OFFSET = 10;
+
+  const angleInRadians = (windDegree - 90) * (Math.PI / 180);
+  const x = RADIUS + (RADIUS - DOT_OFFSET) * Math.cos(angleInRadians);
+  const y = RADIUS + (RADIUS - DOT_OFFSET) * Math.sin(angleInRadians);
+
+  const { temperatureUnit } = useAppSelector((state) => state.weather);
+
+  return (
+    <div className="w-full bg-background p-4 rounded-3xl border border-separate">
+      <div className="flex justify-between items-center w-full gap-2">
+        {/* Wind Details */}
+        <div className="flex flex-col gap-3 w-full">
+          <div className="flex items-center justify-between">
+            <Text size="xs">Wind</Text>
+            <Text size="xs" weight="light" color="muted">
+              {temperatureUnit === "celsius"
+                ? `${windKph} km/h`
+                : `${windMph} mph`}
+            </Text>
+          </div>
+
+          <div className="flex items-center justify-between">
+            <Text size="xs">Gust</Text>
+            <Text size="xs" weight="light" color="muted">
+              {temperatureUnit === "celsius"
+                ? `${gustKph ?? 0} km/h`
+                : `${gustMph ?? 0} mph`}
+            </Text>
+          </div>
+
+          <div className="flex items-center justify-between">
+            <Text size="xs">Direction</Text>
+            <Text size="xs" weight="light" color="muted">
+              {windDegree}° W
+            </Text>
+          </div>
+        </div>
+
+        {/* Wind Compass */}
+        <div className="flex items-center justify-end min-w-24">
+          <div className="relative min-w-24 min-h-24 rounded-full border-2 border-muted bg-background flex items-center justify-center">
+            {/* Cardinal Directions */}
+            <div className="absolute inset-0 text-[10px] font-semibold text-muted-foreground pointer-events-none">
+              <Text
+                size={"xs"}
+                weight={"light"}
+                className="absolute top-0.5 left-1/2 -translate-x-1/2"
+              >
+                N
+              </Text>
+              <Text
+                size={"xs"}
+                weight={"light"}
+                className="absolute bottom-0.5 left-1/2 -translate-x-1/2"
+              >
+                S
+              </Text>
+              <Text
+                size={"xs"}
+                weight={"light"}
+                className="absolute left-0.5 top-1/2 -translate-y-1/2"
+              >
+                W
+              </Text>
+              <Text
+                size={"xs"}
+                weight={"light"}
+                className="absolute right-0.5 top-1/2 -translate-y-1/2"
+              >
+                E
+              </Text>
+            </div>
+
+            {/* Wind Degree Dot */}
+            <div
+              className="absolute size-2 rounded-full bg-chart-1 shadow"
+              style={{
+                left: `${x}px`,
+                top: `${y}px`,
+                transform: "translate(-50%, -50%)",
+              }}
+            />
+
+            {/* Center Wind Speed Display */}
+            <div className="absolute w-12 h-12 rounded-full bg-secondary shadow flex flex-col items-center justify-center z-10 text-[10px] font-medium border">
+              <span className="text-sm leading-none">
+                {temperatureUnit === "celsius" ? windKph : windMph}
+              </span>
+              <span className="text-[9px] text-muted-foreground">
+                {temperatureUnit === "celsius" ? "kmh" : "mph"}
+              </span>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
