ab30eafd225739dee46bd69a6cd6940ce4269f86
arundeshan@gmail.com <arundeshan@gmail.com>
2025-06-18 17:50:14 +0530

    refactor: simplify weather prediction logic and enhance data retrieval methods

--- src/types/index.ts
+++ src/types/index.ts
@@ -1,11 +1,3 @@
-import type { ForecastDay } from "./weather";
-
-export type HourlyPrediction<K extends keyof ForecastDay["hour"][0]> = {
-  time: string;
-} & {
-  [key in K]: ForecastDay["hour"][0][key];
-};
-
 export interface HourlyWeatherData {
   time: string;
   temp: number;
